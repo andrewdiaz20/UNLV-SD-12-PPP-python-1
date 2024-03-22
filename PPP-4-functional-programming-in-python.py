8eaed05db750d6f6cd4e5d3fa32af60fc6a06718
Andrew Diaz <andrewdiazn20@gmail.com>
2024-03-21 21:25:13 -0700

    fith commit

---
+++ PPP-4-functional-programming-in-python.py
@@ -0,0 +1,4 @@
+# Unsorted_list_of_tuples = Unsorted_list_of_tuples.sort[('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)] Sorted list of tuples: [('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
+
+
+# input = {'The': 3, 'quick': 5, 'brown': 5, 'fox': 3, 'jumped': 6, 'over': 4, 'the': 3, 'fence.': 6} for input in range(5)
